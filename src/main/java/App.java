b253734207b827fb14f601a05ad098ee491660de
themerrymarlin <dam134@case.edu>
2017-02-17 14:30:39 -0500

    updated pom file. built simple hello world for localhost

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,11 @@
 /**
  * Created by David Merriman on 02/17/2017.
  */
+
+import static spark.Spark.*;
+
 public class App {
+    public static void main(String[] args){
+        get("/",(req,res)->"hello world");
+    }
 }
