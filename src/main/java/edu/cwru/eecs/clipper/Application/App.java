15934dc674c8574e19c1a0699bcfd66f8c0adb1c
themerrymarlin <dam134@case.edu>
2017-03-02 00:30:55 -0500

    added routes to app. need to create generalized mongo queries so those routes can access their desired info and then return that as JSON

--- src/main/java/edu/cwru/eecs/clipper/Application/App.java
+++ src/main/java/edu/cwru/eecs/clipper/Application/App.java
@@ -1,6 +1,7 @@
 package edu.cwru.eecs.clipper.Application;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
 
 import java.util.HashMap;
@@ -17,50 +18,198 @@ import spark.template.velocity.VelocityTemplateEngine;
 
 public class App {
 
-  public static void main(String[] args) {
-	staticFileLocation("/public");
-    get("/", (req, res) -> generateFeedback());
-    get("/experiment_reset", (req, res) -> experimentalReset());
-    get("/create_user_test", (req, res) -> createUser(res));
-    get("/get_user_test", (req, res) -> getUser(req));
-    get("/*", (req, res) -> render(new HashMap<>(), "/templates/" + req.splat()[0] + ".html"));
-  }
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        get("/", (req, res) -> generateFeedback());
+        get("/experiment_reset", (req, res) -> experimentalReset());
+        get("/create_user_test", (req, res) -> createUser(res));
+        get("/get_user_test", (req, res) -> getUser(req));
+        get("/*", (req, res) -> render(new HashMap<>(), "/templates/" + req.splat()[0] + ".html"));
 
-  private static String render(Map<String, Object> model, String template) {
-    return new VelocityTemplateEngine().render(new ModelAndView(model, template));
-  }
+        //ajax requests from tyler (these will probably need changing as they are rudimentary)
+        //TODO write general mongo functions (probably 2-3) to handle all of these routes
 
-  private static String generateFeedback() {
-    System.out.println("Generating feedback");
-    String feedback = "Hello!";
-    InitialDataManager initialDataManager = new InitialDataManager();
-    int sequence = (int) (Math.random() * 1000);
-    String id = initialDataManager.write("" + sequence);
-    feedback = feedback + ("\nInserted SEQ# = " + sequence);
-    feedback = feedback + ("\nInserted @" + id);
-    String seqout = initialDataManager.read(id);
-    feedback = feedback + ("\nSaved" + seqout);
-    return feedback;
-  }
 
-  private static String experimentalReset() {
-    InitialDataManager.CreateCollection();
-    return "resetted";
-  }
+        //FOOD & ENTERTAINMENT ROUTES
+        get("foodAndEntertainments/:id", (req, res) -> {
+            //TODO hook this up to a mongo call for specific f&e obj
+            return null;
+        });
 
-  private static String createUser(Response res){
-    UserAccountDataManager userAccountDataManager = new UserAccountDataManager();
-    UserAccount account = userAccountDataManager.createUserAccount();
-    System.out.println(MoreObjects.toStringHelper(account));
-    account.setEmail("zxh108@test.case.edu");
-    System.out.println(MoreObjects.toStringHelper(account));
-    userAccountDataManager.updateUserAccount(account);
-    res.redirect("/get_user_test?u="+account.getUserId());
-    return account.toString();
-  }
+        get("foodAndEntertainments", (req, res) -> {
+            //TODO hook up to mongo call for list of food/entertainment objects?
+            return null;
+        });
 
-  private static String getUser(Request request){
-    String userId = request.queryMap("u").value();
-    return new UserAccountDataManager().findByUserID(userId).toString();
-  }
+        post("foodAndEntertainments/search", (req, res) -> {
+            //TODO hook up to mongo call to store for database?
+            return null;
+        });
+
+
+        //EVENT ROUTES
+        get("events/:id", (req, res) -> {
+            //TODO hook this up to a mongo call for specific f&e obj
+            return null;
+        });
+
+        get("events", (req, res) -> {
+            //TODO hook up to mongo call for list of food/entertainment objects?
+            return null;
+        });
+
+        post("events/search", (req, res) -> {
+            //TODO hook up to mongo call to store for database?
+            return null;
+        });
+
+        post("events/create", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        post("events/update", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        post("events/delete", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+
+        //STUDY ROOM ROUTES
+        get("studyRooms/:id", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        get("studyRooms", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        post("studyRooms/create", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        post("studyRooms/update", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        post("studyRooms/delete", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        post("studyRooms/search", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+
+        //BUILDING ROUTES
+        get("buildings/:id", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        get("buildings", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        post("buildings/create", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        post("buildings/update", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        post("buildings/delete", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        post("buildings/search", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+
+        //QUESTION ROUTES
+        get("questions/:id/bestAnswer", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        get("questions/:id", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        get("questions", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        post("questions/create", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        post("questions/update", (req, res) -> {
+            //TODO
+            return null;
+        });
+
+        post("questions/search", (req, res) -> {
+            //TODO
+            return null;
+        });
+    }
+
+    private static String render(Map<String, Object> model, String template) {
+        return new VelocityTemplateEngine().render(new ModelAndView(model, template));
+    }
+
+    private static String generateFeedback() {
+        System.out.println("Generating feedback");
+        String feedback = "Hello!";
+        InitialDataManager initialDataManager = new InitialDataManager();
+        int sequence = (int) (Math.random() * 1000);
+        String id = initialDataManager.write("" + sequence);
+        feedback = feedback + ("\nInserted SEQ# = " + sequence);
+        feedback = feedback + ("\nInserted @" + id);
+        String seqout = initialDataManager.read(id);
+        feedback = feedback + ("\nSaved" + seqout);
+        return feedback;
+    }
+
+    private static String experimentalReset() {
+        InitialDataManager.CreateCollection();
+        return "resetted";
+    }
+
+    private static String createUser(Response res) {
+        UserAccountDataManager userAccountDataManager = new UserAccountDataManager();
+        UserAccount account = userAccountDataManager.createUserAccount();
+        System.out.println(MoreObjects.toStringHelper(account));
+        account.setEmail("zxh108@test.case.edu");
+        System.out.println(MoreObjects.toStringHelper(account));
+        userAccountDataManager.updateUserAccount(account);
+        res.redirect("/get_user_test?u=" + account.getUserId());
+        return account.toString();
+    }
+
+    private static String getUser(Request request) {
+        String userId = request.queryMap("u").value();
+        return new UserAccountDataManager().findByUserID(userId).toString();
+    }
 }
